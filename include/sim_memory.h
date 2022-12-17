f8ab59446608088c4bb4b2deb63da9199bb720bf
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2022-12-17 16:50:24 +0300

    Add memory dump to file and segments loading

--- include/sim_memory.h
+++ include/sim_memory.h
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <array>
-constexpr unsigned DRAM_LEN = 4096;
+constexpr unsigned DRAM_LEN = 4096 * 1024;
 
 namespace sim {
 
 class Memory {
-    std::array<std::byte, DRAM_LEN> addrSpace;
+    char* addrSpace;
+    uint64_t offset = 0x0;
 public:
-    Memory() = default;
+    Memory() {
+        addrSpace = (char*) calloc(DRAM_LEN, sizeof(char));
+    }
+
     void fetchInstruction(uint64_t addr);
     uint64_t load(uint64_t addr);
     void  store(uint64_t  addr);
     int loadELF(std::string& elfFileName);
+    void dump();
+
+    ~Memory() {
+        free(addrSpace);
+    }
 };
 
 } // namespace sim
--- sources/memory/sim_memory.cpp
+++ sources/memory/sim_memory.cpp
@@ -22,16 +22,46 @@ namespace sim {
         std::cout << "Number of segments: " << segNum << std::endl;
 
         std::cout << "\t\tflags\tvaddr\tfsize\tmemory_size"<< std::endl;
+
         unsigned counter = 0;
+
         for (const auto& segment: reader.segments) {
-            std::cout << "Segment [" << counter++ << "]:" << "\t";
+            std::cout << "Segment [" << counter << "]:" << "\t";
             std::cout << std::hex << "0x" << segment->get_flags() << "\t";
             std::cout << std::hex << "0x" << segment->get_virtual_address() << "\t";
             std::cout << std::hex << "0x" << segment->get_file_size() << "\t";
             std::cout << std::hex << "0x" << segment->get_memory_size() << std::endl;
+            
+            auto segment_type = segment->get_type();
+            std::cout << "Segment type: " << std::hex << "0x" << segment->get_type() << std::endl;
+            std::cout << "Segment memory size: " << std::dec << segment->get_memory_size() << std::endl;
+
+            if (segment_type == ELFIO::PT_LOAD) {
+                auto segmentData = segment->get_data();
+                auto segmentDataLen = segment->get_memory_size();
+                std::cout << "Segment data length: " << segmentDataLen << std::endl;
+                    std::cout << "INFO: loading segment " << counter << " into memory..." << std::endl;
+                memcpy(addrSpace + offset, segmentData, segmentDataLen);
+                offset += segmentDataLen;
+            }
+
+            counter++;
         }
 
+        dump();
+
         return 0;
+    }
+
+    void Memory::dump() {
+        std::cout << "Memory offset: " << offset << std::endl;
+        std::cout << "Dumping memory starting from address " << std::hex << "0x0 " << std::endl;
+
+        std::ofstream dumpFile("memory_dump.hex");
+        if (dumpFile.is_open()) {
+            dumpFile.write(addrSpace, offset);
+            dumpFile.close();
+        }   
     }
 
 } // namespace sim
