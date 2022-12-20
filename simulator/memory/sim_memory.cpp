d3adc78c5fd20e683f2ff86257f72c7260035bb9
Stanislav Sidelnikov <sidelnikov.si.001@gmail.com>
2022-12-20 11:43:12 +0300

    Add memory permissions control function

--- simulator/memory/sim_memory.cpp
+++ simulator/memory/sim_memory.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <iostream>
 #include <elfio/elfio.hpp>
+#include <elf.h>
 
 #include "sim_memory.h"
 
@@ -24,6 +25,41 @@ namespace sim {
 
     void Memory::upload(size_t offset, const char* data, size_t len) {
         memcpy(addrSpace_ + offset, data, len);
+    }
+
+    unsigned Memory::getAccessRegion(size_t offset) {
+        unsigned regionID = 0;
+        for (const auto& regionBorders: memoryRegions) {
+            auto&[left, right] = regionBorders.second;
+            regionID = regionBorders.first;
+            if (offset >= left && offset <= right) {
+                return regionID;
+            }
+        }
+        std::cerr << "ERROR: memory region with offset" << offset << "not found" << std::endl;
+        throw std::runtime_error("Memory region initialization failure");
+    }
+
+    void Memory::accessMemory(size_t offset, size_t nbytes, enum accessModes accessMode) {
+        auto regionID = getAccessRegion(offset);
+        auto segmentPermissions = regionPermissions.at(regionID);
+        auto nextRegionID = getAccessRegion(offset + nbytes);
+        if (regionID != nextRegionID) {
+            throw std::runtime_error("ERROR: memory access range overflows segment");
+        }
+        if (offset + nbytes > DRAM_SIZE) {
+            throw std::runtime_error("ERROR: invalid memory access range");
+
+        }
+        if (accessMode == accessModes::READ) {
+            if (segmentPermissions != PF_R) {
+                throw std::runtime_error("ERROR: accessing memory: permission denied");
+            }
+        } else if (accessMode == accessModes::WRITE) {
+            if (segmentPermissions != PF_W) {
+                throw std::runtime_error("ERROR: accessing memory: permission denied");
+            }
+        }
     }
 
 } // namespace sim
